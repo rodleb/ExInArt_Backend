20f2f41565298c9c61f332e66b6e4ad55b89ddf1
rodleb <57004543+rodleb@users.noreply.github.com>
2023-09-07 00:04:33 +0300

    Update settings.py
    Added Static root

--- xnart/settings.py
+++ xnart/settings.py
@@ -29,6 +29,8 @@ fire_base_url = 'exinart-13556.appspot.com'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
