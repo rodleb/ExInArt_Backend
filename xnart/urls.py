95ee805d24c2122e8e066cc69d70afad1128d8ae
rodleb <rodleb@yahoo.com>
2023-09-11 12:47:03 +0300

    some extra fixes

--- xnart/urls.py
+++ xnart/urls.py
@@ -13,15 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import include
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from rest_framework.schemas import get_schema_view
 from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 # from .views import protected_view
-from django.conf.urls import url
-from rest_framework_swagger.views import get_swagger_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
@@ -45,12 +43,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('user/', include('users_api.urls')),  # Include your users_api app's URLs
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('network/', include('user_network_api.urls')),  # Include your user_network_api app's URLs
+    path('api/admin/', admin.site.urls),
+    path('api/user/', include('users_api.urls')),  # Include your users_api app's URLs
+    path('api/swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('api/network/', include('user_network_api.urls')),  # Include your user_network_api app's URLs
     #JWT token refresh and shit
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # url(r'^api-token-verify/', verify_jwt_token),
 ]
