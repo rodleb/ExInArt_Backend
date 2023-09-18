bbf261780d9f642a7a2df4e2a39d5e76799e47e9
rodleb <rodleb@yahoo.com>
2023-09-18 15:22:01 +0300

    fixes on index html file

--- xnart/settings.py
+++ xnart/settings.py
@@ -36,6 +36,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'build/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 # Quick-start development settings - unsuitable for production
